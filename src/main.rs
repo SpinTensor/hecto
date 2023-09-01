713374580a6e40eb43f27adb3e17e140bd00ca1e
Felix Uhl <felix.uhl@rub.de>
2023-09-01 08:42:25 +0200

    moved to key input instead of bytes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
 use termion::raw::IntoRawMode;
+use termion::event::Key;
+use termion::input::TermRead;
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
 
 fn main() {
    let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
-        if c == 'q' {
-            break;
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?} \r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
+                },
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
+            },
+            Err(err) => die(err),
         }
     }
 }
